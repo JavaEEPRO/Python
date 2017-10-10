5edb3164fd1153fb17c2b0f723ad4033262e2a0f
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-10 23:21:09 +0300

    String Formatting with %, Part 1
     Blessed [is] he that readeth, and they that hear the words of this prophecy, and keep those things which are written therein: for the time [is] at hand. (Revelation 1:3)
    
    STRINGS & CONSOLE OUTPUT
    String Formatting with %, Part 1
    When you want to print a variable with a string, there is a better method than concatenating strings together.
    
    name = "Mike"
    print "Hello %s" % (name)
    The % operator after a string is used to combine a string with variables. The % operator will replace a %s in the string with the string variable that comes after it.
    Instructions
    1.
    Take a look at the code in the editor. What do you think it'll do? Click Run when you think you know.

---
+++ stringFormattingWith%.py
@@ -0,0 +1,24 @@
+ # Blessed [is] he that readeth, and they that hear the words of this prophecy, 
+ # and keep those things which are written therein: for the time [is] at hand. (Revelation 1:3)
+ 
+ string_1 = "Camelot"
+string_2 = "place"
+
+print "Let's not go to %s. 'Tis a silly %s." % (string_1, string_2)
+
+
+
+"""
+
+STRINGS & CONSOLE OUTPUT
+String Formatting with %, Part 1
+When you want to print a variable with a string, there is a better method than concatenating strings together.
+
+name = "Mike"
+print "Hello %s" % (name)
+The % operator after a string is used to combine a string with variables. The % operator will replace a %s in the string with the string variable that comes after it.
+Instructions
+1.
+Take a look at the code in the editor. What do you think it'll do? Click Run when you think you know.
+
+"""
