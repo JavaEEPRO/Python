721e71895a4ff0b6ac1f780f1f8cd9ccaf8727bf
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-10 22:34:51 +0300

    Dot Notation
    The Revelation of Jesus Christ, which God gave unto him, to shew unto his servants things which must shortly come to pass; and he sent and signified [it] by his angel unto his servant John (Revelation 1:1)
    
    STRINGS & CONSOLE OUTPUT
    Dot Notation
    Let's take a closer look at why you use len(string) and str(object), but dot notation (such as "String".upper()) for the rest.
    
    lion = "roar"
    len(lion)
    lion.upper()
    Methods that use dot notation only work with strings.
    
    On the other hand, len() and str() can work on other data types.
    Instructions
    1.
    On line 3, call the len() function with the argument ministry.
    
    On line 4, invoke the ministry's .upper() function.

---
+++ dotNotation.py
@@ -0,0 +1,29 @@
+
+# The Revelation of Jesus Christ, which God gave unto him, to shew unto his servants things which must shortly come to pass; 
+# and he sent and signified [it] by his angel unto his servant John (Revelation 1:1)
+
+ministry = "The Ministry of Silly Walks"
+
+print len(ministry)
+print ministry.upper()
+
+
+"""
+
+STRINGS & CONSOLE OUTPUT
+Dot Notation
+Let's take a closer look at why you use len(string) and str(object), but dot notation (such as "String".upper()) for the rest.
+
+lion = "roar"
+len(lion)
+lion.upper()
+Methods that use dot notation only work with strings.
+
+On the other hand, len() and str() can work on other data types.
+Instructions
+1.
+On line 3, call the len() function with the argument ministry.
+
+On line 4, invoke the ministry's .upper() function.
+
+"""
