1c53ef8e424bb63da6dd1619c4571a96754f7553
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-13 23:21:03 +0300

    not
    But though he had done so many signs before them, yet they didn't believe in him (John 12:37)
    
    CONDITIONALS & CONTROL FLOW
    Not
    The boolean operator not returns True for false statements and False for true statements.
    
    For example:
    
    not False will evaluate to True, while not 41 > 40 will return False.
    1.
    Let's get some practice with not.
    
    Set bool_one equal to the result of
    not True
    Set bool_two equal to the result of
    not 3 ** 4 < 4 ** 3
    Set bool_three equal to the result of
    not 10 % 3 <= 10 % 2
    Set bool_four equal to the result of
    not 3 ** 2 + 4 ** 2 != 5 ** 2
    Set bool_five equal to the result of
    not not False

---
+++ conditionalsAndControlFlow/not.py
@@ -0,0 +1,40 @@
+
+# But though he had done so many signs before them, yet they didn't believe in him (John 12:37)
+
+
+
+bool_one = False
+
+bool_two = True
+
+bool_three = True
+
+bool_four = True
+
+bool_five = False
+
+
+"""
+
+CONDITIONALS & CONTROL FLOW
+Not
+The boolean operator not returns True for false statements and False for true statements.
+
+For example:
+
+not False will evaluate to True, while not 41 > 40 will return False.
+1.
+Let's get some practice with not.
+
+Set bool_one equal to the result of
+not True
+Set bool_two equal to the result of
+not 3 ** 4 < 4 ** 3
+Set bool_three equal to the result of
+not 10 % 3 <= 10 % 2
+Set bool_four equal to the result of
+not 3 ** 2 + 4 ** 2 != 5 ** 2
+Set bool_five equal to the result of
+not not False
+
+"""
