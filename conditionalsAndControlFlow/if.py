a2f4599333e3a76b343ca2ef8f774ca0462418d6
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-15 23:45:00 +0300

    where I am, you may be there also
    If I go and prepare a place for you, I will come again, and will receive you to myself; that where I am, you may be there also. (John 14:3)
    
    CONDITIONALS & CONTROL FLOW
    Conditional Statement Syntax
    if is a conditional statement that executes some specified code after checking if its expression is True.
    
    Here's an example of if statement syntax:
    
    if 8 < 9:
      print "Eight is less than nine!"
    In this example, 8 < 9 is the checked expression and print "Eight is less than nine!" is the specified code.
    1.
    If you think the print statement will print to the console, set response equal to 'Y'; otherwise, set response equal to 'N'.

---
+++ conditionalsAndControlFlow/if.py
@@ -0,0 +1,33 @@
+
+
+# If I go and prepare a place for you, I will come again, and will receive you to myself;
+# that where I am, you may be there also. (John 14:3)
+
+
+
+response = 'Y'
+
+answer = "Left"
+if answer == "Left":
+    print "This is the Verbal Abuse Room, you heap of parrot droppings!"
+    
+# Will the above print statement print to the console?
+# Set response to 'Y' if you think so, and 'N' if you think not.
+
+
+
+"""
+
+CONDITIONALS & CONTROL FLOW
+Conditional Statement Syntax
+if is a conditional statement that executes some specified code after checking if its expression is True.
+
+Here's an example of if statement syntax:
+
+if 8 < 9:
+  print "Eight is less than nine!"
+In this example, 8 < 9 is the checked expression and print "Eight is less than nine!" is the specified code.
+1.
+If you think the print statement will print to the console, set response equal to 'Y'; otherwise, set response equal to 'N'.
+
+"""
