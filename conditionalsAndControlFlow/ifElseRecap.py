617e8c0101862c0ef26ccdb1056faa02aea0cb2b
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-17 23:30:14 +0300

    keep my word
    Jesus answered, and said to him: If any one love me, he will keep my word, and my Father will love him, and we will come to him, and will make our abode with him. (John 14:23)

---
+++ conditionalsAndControlFlow/ifElseRecap.py
@@ -0,0 +1,55 @@
+
+# Jesus answered, and said to him: If any one love me, he will keep my word, and my Father will love him, 
+# and we will come to him, and will make our abode with him. (John 14:23)
+
+
+
+# Make sure that the_flying_circus() returns True
+def the_flying_circus():
+    if 3 == 3 and not 7 < 6 :    # Start coding here!
+       return True # Don't forget to indent
+        # the code inside this block!
+    elif 7 > 5 or 3 > 2 :
+        return False# Keep going here.
+        # You'll want to add the else statement, too!
+    else :
+      	return True
+    
+
+
+
+"""
+CONDITIONALS & CONTROL FLOW
+The Big If
+Really great work! Here's what you've learned in this unit:
+
+Comparators
+
+3 < 4
+5 >= 5
+10 == 10
+12 != 13
+Boolean operators
+
+True or False 
+(3 < 4) and (5 >= 5)
+this() and not that()
+Conditional statements
+
+if this_might_be_true():
+  print "This really is true."
+elif that_might_be_true():
+  print "That is true."
+else:
+  print "None of the above."
+Let's get to the grand finale.
+1.
+Write an if statement in the_flying_circus(). It must include:
+
+if, elif, and else statements;
+At least one of and, or, or not;
+A comparator (==, !=, <, <=, >, or >=);
+Finally, the_flying_circus() must return True when evaluated.
+Don't forget to include a : after your if statements!
+
+"""
