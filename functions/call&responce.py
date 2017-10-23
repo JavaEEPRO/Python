3b5ba4b4100de93404a5dff82e19977eb35cf694
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-10-23 12:16:41 +0300

    SpiritOfTruth,has come,he'll guide u intoAll truth
    However when he, the Spirit of truth, has come, he will guide you into all truth, for he will not speak from himself; but whatever he hears, he will speak. He will declare to you things that are coming. (John 16:13)

---
+++ functions/call&responce.py
@@ -0,0 +1,27 @@
+
+# However when he, the Spirit of truth, has come, he will guide you into all truth, for he will not speak from himself; but whatever he hears, he will speak. He will declare to you things that are coming. (John 16:13)
+
+
+def square(n):
+  """Returns the square of a number."""
+  squared = n ** 2
+  print "%d squared is %d." % (n, squared)
+  return squared
+  
+# Call the square function on line 10! Make sure to
+# include the number 10 between the parentheses.
+
+square(10)
+
+
+
+
+"""
+FUNCTIONS
+Call and Response
+After defining a function, it must be called to be implemented. In the previous exercise, spam() in the last line told the program to look for the function called spam and execute the code inside it.
+Instructions
+1.
+We've set up a function, square. Call it on the number 10 (by putting 10 between the parentheses of square()) on line 9!
+
+"""
