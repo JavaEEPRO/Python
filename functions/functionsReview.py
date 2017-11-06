63b096195fbb72accc3789fa2ccb02ae3c3d4056
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-11-06 10:00:36 +0200

    Jesus came& stood in midst,&said 2them, Peace be2u
    When therefore it was evening, on that day, the first day of the week, and when the doors were locked where the disciples were assembled, for fear of the Jews, Jesus came and stood in the midst, and said to them, "Peace be to you." (John 20:19)

---
+++ functions/functionsReview.py
@@ -0,0 +1,43 @@
+
+
+# When therefore it was evening, on that day, the first day of the week, 
+# and when the doors were locked where the disciples were assembled, for fear of the Jews, 
+# Jesus came and stood in the midst, and said to them, "Peace be to you." (John 20:19)
+
+
+def distance_from_zero(arg):
+  if type(arg) == int or type(arg) == float:
+    return abs(arg)
+  else:
+    return "Nope"
+
+
+
+"""
+FUNCTIONS
+Review: Built-In Functions
+Perfect! Last but not least, let's review the built-in functions you've learned about in this lesson.
+
+def is_numeric(num):
+  return type(num) == int or type(num) == float:
+
+max(2, 3, 4) # 4
+min(2, 3, 4) # 2
+
+abs(2) # 2
+abs(-2) # 2
+1.
+First, def a function called distance_from_zero, with one argument (choose any argument name you like).
+
+If the type of the argument is either int or float, the function should return the absolute value of the function input.
+
+Otherwise, the function should return "Nope"
+Stuck? Get a hint
+
+Community Forums
+Get help and ask questions on the Codecademy community forums.
+
+Go to the forums
+
+
+"""
