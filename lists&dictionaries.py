b2cd34c63bc543e04f7977eaa17662282a2bc189
Volodymyr Shtoda <30345665+JavaEEPRO@users.noreply.github.com>
2017-11-25 09:46:00 +0200

    thou doest these things
    Then answered the Jews and said unto him, What sign shewest thou unto us, seeing that thou doest these things? (John 2:19)

---
+++ lists&dictionaries.py
@@ -0,0 +1,45 @@
+
+# Then answered the Jews and said unto him, What sign shewest thou unto us, seeing that thou doest these things? (John 2:19)
+
+menu = {} # Empty dictionary
+menu['Chicken Alfredo'] = 14.50 # Adding new key-value pair
+print menu['Chicken Alfredo']
+
+# Your code here: Add some dish-price pairs to menu!
+infinity = 123456789
+menu['BodyOfJesus'] = infinity
+menu['BloodOfJesus'] = infinity
+menu['AliveWater'] = infinity
+print "There are " + str(len(menu)) + " items on the menu."
+print menu
+
+
+
+"""
+PYTHON LISTS AND DICTIONARIES
+New Entries
+Like Lists, Dictionaries are mutable. This means they can be changed after they are created. One advantage of this is that we can add new key/value pairs to the dictionary after it is created like so:
+
+dict_name[new_key] = new_value
+An empty pair of curly braces {} is an empty dictionary, just like an empty pair of [] is an empty list.
+
+The length len() of a dictionary is the number of key-value pairs it has. Each pair counts only once, even if the value is a list. (That's right: you can put lists inside dictionaries!)
+1.
+Add at least three more key-value pairs to the menu variable, with the dish name (as a "string") for the key and the price (a float or integer) as the value. Here's an example:
+
+menu['Spam'] = 2.50
+
+
+menu = {} # Empty dictionary
+menu['Chicken Alfredo'] = 14.50 # Adding new key-value pair
+print menu['Chicken Alfredo']
+
+# Your code here: Add some dish-price pairs to menu!
+
+
+
+
+print "There are " + str(len(menu)) + " items on the menu."
+print menu
+
+"""
